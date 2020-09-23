f67579ca36a29d5a4543de68832fa1fc78fa4d03
Gabriel Bustamante <gabriel.bustamante@usach.cl>
2020-09-23 17:51:49 -0300

    Primeras funciones en front para conectar con back

--- FilaVirtual-FrontEnd/fila-virtual/src/store/index.js
+++ FilaVirtual-FrontEnd/fila-virtual/src/store/index.js
@@ -1,12 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
 
 export default new Vuex.Store({
   state: {
+    users: []
   },
   mutations: {
+
+    getAllUsers(state) {
+			try {
+			axios.get('http://localhost:8081/user/getall/')
+				.then(response => {
+					//Se guarda la respuesta que envía el backend en la variable users.
+					state.users = response.data;
+					})
+				.catch(e => {
+					this.errors.push(e)
+					})
+			} catch (err) {
+
+				console.log("Se obtuvo el siguiente error al traer los usuarios: " + err)
+			}
+		},
+
+    createUser(state, new_user) {
+      try {
+				axios.post('http://localhost:8081/user/registeruser/', new_user)
+					.then(response => {
+						console.log(response.status)
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			} catch (err) {
+				console.log("Se obtuvo el siguiente error al traer los usuarios: " + err)
+			}
+    }
+
   },
   actions: {
   },
